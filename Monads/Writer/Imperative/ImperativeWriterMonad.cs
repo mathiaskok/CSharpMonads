f68b189ca20046ed22789ad37f36ad3d3f27d173
Mathias <mathias.kok@gmail.com>
2017-11-20 21:57:20 +0100

    Added ImperativeWriterMonad.

---
+++ Monads/Writer/Imperative/ImperativeWriterMonad.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monads.Writer.Imperative
+{
+  public class ImperativeWriterMonad<T>
+  {
+    private Action<string> WriterAction { get; }
+    public T Value { get; }
+
+    private ImperativeWriterMonad(T value, Action<string> writerAction)
+    {
+      Value = value;
+      WriterAction = writerAction;
+    }
+
+    public ImperativeWriterMonad<T> Return(T value, Action<string> writerAction)
+    {
+      return new ImperativeWriterMonad<T>(value, writerAction);
+    }
+
+    public ImperativeWriterMonad<U> Bind<U>(Func<T, (U, IEnumerable<string>)> binder)
+    {
+      var (u, w) = binder(Value);
+      foreach (string m in w)
+        WriterAction(m);
+
+      return new ImperativeWriterMonad<U>(u, WriterAction);
+    }
+
+    public ImperativeWriterMonad<U> Bind<U>(Func<T, (U, string)> binder)
+    {
+      var (u, w) = binder(Value);
+      WriterAction(w);
+
+      return new ImperativeWriterMonad<U>(u, WriterAction);
+    }
+
+    public ImperativeWriterMonad<U> Bind<U>(Func<T, Tuple<U, IEnumerable<string>>> binder)
+    {
+      var (u, w) = binder(Value);
+      foreach (string m in w)
+        WriterAction(m);
+
+      return new ImperativeWriterMonad<U>(u, WriterAction);
+    }
+
+    public ImperativeWriterMonad<U> Bind<U>(Func<T, Tuple<U, string>> binder)
+    {
+      var (u, w) = binder(Value);
+      WriterAction(w);
+
+      return new ImperativeWriterMonad<U>(u, WriterAction);
+    }
+
+    public ImperativeWriterMonad<U> Map<U>(Func<T, U> mapper)
+    {
+      return new ImperativeWriterMonad<U>(mapper(Value), WriterAction);
+    }
+  }
+}
