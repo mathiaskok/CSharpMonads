264fc0cd3a3322e0cbabdc4129a965a6c8c4f3a1
Mathias <mathias.kok@gmail.com>
2017-11-22 07:19:43 +0100

    Started StateMonads.

---
+++ Monads/State/Lazy/LazyStateMonad.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monads.State.Lazy
+{
+  public class LazyStateMonad<TValue, TState>
+  {
+    public Func<TState, (TValue, TState)> StateFunction { get; }
+
+    private LazyStateMonad(Func<TState, (TValue, TState)> stateFunction)
+    {
+      StateFunction = stateFunction;
+    }
+
+    public LazyStateMonad<UValue, TState> Bind<UValue>(Func<TValue, Func<TState, (UValue, TState)>> binder)
+    {
+      Func<TState, (UValue, TState)> func = s =>
+      {
+        (var tv, var ts) = StateFunction(s);
+        return binder(tv)(ts);
+      };
+
+      return new LazyStateMonad<UValue, TState>(func);
+    }
+  }
+}
---
+++ Monads/State/Strict/StrictStateMonad.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monads.State.Strict
+{
+  public class StrictStateMonad<TValue, TState>
+  {
+    
+  }
+}
