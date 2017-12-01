5813c52cee816fdda7985fff9e6ed8a5ad47c126
Mathias <mathias.kok@gmail.com>
2017-12-01 20:35:32 +0100

    Function Memoization.

---
+++ Monads/FunctionStructures/DelegateExtensions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using static Monads.Comparers.EqualityComparerExtensions;
+
+namespace Monads.FunctionStructures
+{
+  public static class DelegateExtensions
+  {
+    public static Func<T, U> Memoize<T, U>(this Func<T, U> func, IEqualityComparer<T> comparer)
+    {
+      MemoizedFunction<T, U> memoized = new MemoizedFunction<T, U>(func, comparer);
+      return memoized.Apply;
+    }
+
+    public static Func<T, U> Memoize<T, U>(this Func<T, U> func)
+    {
+      return func.Memoize(EqualityComparer<T>.Default);
+    }
+
+    public static Func<T, U, V> Memoize<T, U, V>(
+      this Func<T, U, V> func,
+      IEqualityComparer<T> comparerT,
+      IEqualityComparer<U> comparerU)
+    {
+      return func.Memoize(AsTupleComparer(comparerT, comparerU));
+    }
+
+    public static Func<T, U, V> Memoize<T, U, V>(this Func<T, U, V> func)
+    {
+      return func.Memoize(EqualityComparer<(T, U)>.Default);
+    }
+
+    private static Func<T, U, V> Memoize<T, U, V>(
+      this Func<T, U, V> func,
+      IEqualityComparer<(T, U)> comparer)
+    {
+      MemoizedFunction<(T, U), V> memoized = new MemoizedFunction<(T, U), V>(
+        t => func(t.Item1, t.Item2),
+        comparer);
+
+      return (t, u) => memoized.Apply((t, u));
+    }
+
+    public static Func<T, U, V, X> Memoize<T, U, V, X>(
+      this Func<T, U, V, X> func,
+      IEqualityComparer<T> comparerT,
+      IEqualityComparer<U> comparerU,
+      IEqualityComparer<V> comparerV)
+    {
+      return func.Memoize(AsTupleComparer(comparerT, comparerU, comparerV));
+    }
+
+    public static Func<T, U, V, X> Memoize<T, U, V, X>(this Func<T, U, V, X> func)
+    {
+      return func.Memoize(EqualityComparer<(T, U, V)>.Default);
+    }
+
+    private static Func<T, U, V, X> Memoize<T, U, V, X>(
+      this Func<T, U, V, X> func,
+      IEqualityComparer<(T, U, V)> comparer)
+    {
+      MemoizedFunction<(T, U, V), X> memoized = new MemoizedFunction<(T, U, V), X>(
+        t => func(t.Item1, t.Item2, t.Item3),
+        comparer);
+
+      return (t, u, v) => memoized.Apply((t, u, v));
+    }
+  }
+}
---
+++ Monads/FunctionStructures/MemoizedFunction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monads.FunctionStructures
+{
+  public class MemoizedFunction<T, U>
+  {
+    private readonly Func<T, U> MemoizedFuntion;
+    private readonly Dictionary<T, U> MemoizedResults;
+
+    public MemoizedFunction(Func<T, U> func, IEqualityComparer<T> comparer)
+    {
+      MemoizedFuntion = func;
+      MemoizedResults = new Dictionary<T, U>(comparer);
+    }
+
+    public MemoizedFunction(Func<T, U> func)
+      : this(func, EqualityComparer<T>.Default) { }
+
+    public U Apply(T t)
+    {
+      if (MemoizedResults.TryGetValue(t, out U u))
+        return u;
+
+      u = MemoizedFuntion(t);
+      MemoizedResults.Add(t, u);
+      return u;
+    }
+  }
+}
