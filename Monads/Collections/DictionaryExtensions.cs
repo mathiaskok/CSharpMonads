3292a85a3e1f02f4aad45248a83e1e819eb9bd65
Mathias <mathias.kok@gmail.com>
2017-12-01 21:33:14 +0100

    Added ReadOnlyDictionaryAdpater.

---
+++ Monads/Collections/DictionaryExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Monads.Collections.ReadOnly;
+using System.Linq;
+
+namespace Monads.Collections
+{
+  public static class DictionaryExtensions
+  {
+    public static IReadOnlyDictionary<TKey, TValue> AsReadOnly<TKey, TValue>(
+      this IDictionary<TKey, TValue> dict)
+    {
+      return new ReadOnlyDictionaryAdapter<TKey, TValue>(dict);
+    }
+
+    public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(
+      this IEnumerable<(TKey, TValue)> seq,
+      IEqualityComparer<TKey> keyComparer)
+    {
+      return seq.ToDictionary(t => t.Item1, t => t.Item2, keyComparer);
+    }
+
+    public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(
+      this IEnumerable<(TKey, TValue)> seq)
+    {
+      return seq.ToDictionary(EqualityComparer<TKey>.Default);
+    }
+
+    public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(
+      this IEnumerable<Tuple<TKey, TValue>> seq,
+      IEqualityComparer<TKey> keyComparer)
+    {
+      return seq.ToDictionary(t => t.Item1, t => t.Item2, keyComparer);
+    }
+
+    public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(
+      this IEnumerable<Tuple<TKey, TValue>> seq)
+    {
+      return seq.ToDictionary(EqualityComparer<TKey>.Default);
+    }
+
+    public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(
+      this IEnumerable<KeyValuePair<TKey, TValue>> seq,
+      IEqualityComparer<TKey> keyComparer)
+    {
+      return seq.ToDictionary(t => t.Key, t => t.Value, keyComparer);
+    }
+
+    public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(
+      this IEnumerable<KeyValuePair<TKey, TValue>> seq)
+    {
+      return seq.ToDictionary(EqualityComparer<TKey>.Default);
+    }
+  }
+}
---
+++ Monads/Collections/ReadOnly/ReadOnlyDictionaryAdapter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Monads.Collections.ReadOnly
+{
+  public class ReadOnlyDictionaryAdapter<TKey, TValue> : IReadOnlyDictionary<TKey, TValue>
+  {
+    private readonly IDictionary<TKey, TValue> Adaptee;
+
+    public TValue this[TKey key] => Adaptee[key];
+
+    public IEnumerable<TKey> Keys => Adaptee.Keys;
+
+    public IEnumerable<TValue> Values => Adaptee.Values;
+
+    public int Count => Adaptee.Count;
+
+    public ReadOnlyDictionaryAdapter(IDictionary<TKey, TValue> adaptee)
+    {
+      Adaptee = adaptee;
+    }
+
+    public bool ContainsKey(TKey key) => Adaptee.ContainsKey(key);
+
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => Adaptee.GetEnumerator();
+
+    public bool TryGetValue(TKey key, out TValue value) => Adaptee.TryGetValue(key, out value);
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+  }
+}
