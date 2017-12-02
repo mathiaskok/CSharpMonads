6bd87270135cd28d0b7b051015e2f79dbf2faf0c
mathiaskok <mathias.kok@gmail.com>
2017-12-02 14:05:40 +0100

    Added ArrayHashCode.

--- Monads/Comparers/HashCodeExtensions.cs
+++ Monads/Comparers/HashCodeExtensions.cs
@@ -8,31 +8,32 @@ namespace Monads.Comparers
     public const int BaseHashCode = 10007; //Large Prime
     public const int StepHashCode = 486187739; //Large Prime
 
-    public static int HashWith<T>(this int i, T t)
+    public static int HashWith<T>(this int i, T t) =>
+      (i * StepHashCode) + t.GetHashCode();
+
+    public static int HashWith(this int i, int o) =>
+      (i * StepHashCode) + o;
+
+    public static int ArrayHashCode<T>(this T[] arr)
     {
-      return (i * StepHashCode) + t.GetHashCode();
+      int l = arr.Length;
+      int hash = BaseHashCode;
+
+      for (int i = 0; i < l; i++)
+        hash = (hash * StepHashCode) + arr[i].GetHashCode();
+
+      return hash;
     }
 
-    public static int HashWith<T>(this int i, int o)
-    {
-      return (i * StepHashCode) + o;
-    }
+    public static int SequenceHashCode(this IEnumerable<int> seq) =>
+      seq.Aggregate(BaseHashCode, HashWith);
 
-    public static int SequenceHashCode(this IEnumerable<int> seq)
-    {
-      return seq.Aggregate(BaseHashCode, HashWith);
-    }
-
-    public static int SequenceHashCode<T>(this IEnumerable<T> seq, IEqualityComparer<T> comparer)
-    {
-      return seq
+    public static int SequenceHashCode<T>(this IEnumerable<T> seq, IEqualityComparer<T> comparer) =>
+      seq
         .Select(comparer.GetHashCode)
         .SequenceHashCode();
-    }
 
-    public static int SequenceHashCode<T>(this IEnumerable<T> seq)
-    {
-      return seq.SequenceHashCode(EqualityComparer<T>.Default);
-    }
+    public static int SequenceHashCode<T>(this IEnumerable<T> seq) =>
+      seq.SequenceHashCode(EqualityComparer<T>.Default);
   }
 }
