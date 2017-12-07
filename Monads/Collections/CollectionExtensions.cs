0d15caf44aec9b2b13fbdb8c87785302c64364a6
mathiaskok <mathias.kok@gmail.com>
2017-12-07 18:19:42 +0100

    Added ReadOnlyCollection adapter + CollectionExtensions.

---
+++ Monads/Collections/CollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Monads.Collections.ReadOnly;
+
+namespace Monads.Collections
+{
+  public static class CollectionExtensions
+  {
+    public static IReadOnlyCollection<T> AsReadOnly<T>(this IEnumerable<T> seq)
+    {
+      if (seq is IReadOnlyCollection<T> rc)
+        return rc;
+      else if (seq is ICollection<T> c)
+        return new ReadOnlyCollectionAdpater<T>(c);
+      else if (seq is IList<T> l)
+        return new ReadOnlyCollection<T>(l);
+      else
+        return seq.ToList().AsReadOnly();
+    }
+  }
+}
---
+++ Monads/Collections/ReadOnly/ReadOnlyCollectionAdpater.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Monads.Collections.ReadOnly
+{
+  public class ReadOnlyCollectionAdpater<T> : IReadOnlyCollection<T>
+  {
+    private readonly ICollection<T> Adaptee;
+
+    public int Count => Adaptee.Count;
+
+    public ReadOnlyCollectionAdpater(ICollection<T> adaptee)
+    {
+      Adaptee = adaptee;
+    }
+
+    public IEnumerator<T> GetEnumerator() => Adaptee.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+  }
+}
