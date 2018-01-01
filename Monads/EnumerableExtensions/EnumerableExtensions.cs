683de6d2dc996d9c06e749c922dcf6cee2829351
mathiaskok <mathias.kok@gmail.com>
2018-01-01 14:29:33 +0100

    Fixed name collision with (Linq -> Enumerable)

--- Monads/EnumerableExtensions/EnumerableExtensions.cs
+++ Monads/EnumerableExtensions/EnumerableExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Monads.Collections;
 
-namespace Monads.Enumerable
+namespace Monads.EnumerableExtensions
 {
   public static class EnumerableExtensions
   {
